07f87d39506001022cd22bb31f057e225fd5bbf3
ChihChi-Chou <chi20020314@gmail.com>
2025-02-25 20:38:16 +0800

    Create exercise2.11.3.py

---
+++ 250225/exercise2.11.3.py
@@ -0,0 +1,19 @@
+# prat1
+import math
+r = 5
+v1 = math.pow(5,3) * math.pi *4/3
+print('part1 v1=',v1)
+
+# part2
+x=42
+v2 = math.sin(x)**2 + math.cos(x)**2
+print('part2 v2=',v2) 
+
+# part3
+e_1 = math.e**2
+e_2 = math.pow(math.e,2)
+x=2
+e_3 = math.exp(x)
+print('part3 e_1=',e_1) 
+print('part3 e_2=',e_2) 
+print('part3 e_3=',e_3) 
